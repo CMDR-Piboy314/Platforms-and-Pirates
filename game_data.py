8f0a2a95395ae5c15d8cbbb31dcdf3638bede5a6
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-03-16 13:05:38 +0900

    Add music and start work on overworld

--- game_data.py
+++ game_data.py
@@ -1,4 +1,5 @@
-level_0 = {
+# Normal level data
+level_0_data = {
     "terrain":     "res/levels/0/level_0_terrain.csv",
     "coins":       "res/levels/0/level_0_coins.csv",
     "fg palms":    "res/levels/0/level_0_fg_palms.csv",
@@ -8,4 +9,45 @@ level_0 = {
     "constraints": "res/levels/0/level_0_constraints.csv",
     "player":      "res/levels/0/level_0_player.csv",
     "grass":       "res/levels/0/level_0_grass.csv"
+}
+
+# Overworld level data
+level_0 = {
+    "node_pos": (110,  400),
+    "content": "This is level 0",
+    "unlock": 1
+}
+level_1 = {
+    "node_pos": (300,  220),
+    "content": "This is level 1",
+    "unlock": 2
+}
+level_2 = {
+    "node_pos": (480,  610),
+    "content": "This is level 2",
+    "unlock": 3
+}
+level_3 = {
+    "node_pos": (610,  350),
+    "content": "This is level 3",
+    "unlock": 4
+}
+level_4 = {
+    "node_pos": (880,  210),
+    "content": "This is level 4",
+    "unlock": 5
+}
+level_5 = {
+    "node_pos": (1050, 400),
+    "content": "This is level 5",
+    "unlock": 6
+}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
 }
--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 # Programmed by Piboy314 for Constellation Games 🍝
-import pygame, sys, engine, time
+import pygame, sys, engine, time, overworld, game_data
 
 from tiles import Tile
 from level import Level
-from game_data import level_0
 from settings import *
 
 # Define Constants
 FPS_MAX = 60
 
-# Define variables
-
-
 # Create pygame stuff
 pygame.init() # Initialize pygame
 screen = pygame.display.set_mode(WINDOW_SIZE) # Create a window with the size being WINDOW_SIZE
 pygame.display.set_caption("Platforms and Pirates")
+pygame.mixer.music.load("res/wav/Mario.wav")
+pygame.mixer.music.set_volume(0.7)
+
+# Create classes
+class Game:
+    def __init__(self):
+        self.max_level = 6
+        self.overworld = overworld.Overworld(0, self.max_level, screen)
+
+    def run(self):
+        #pass
+        self.overworld.run()
 
 # Create instances
 clock = pygame.time.Clock() # Create a clock for framerate capping and ticks
-level = Level(level_0, screen)
+level = Level(game_data.level_0_data, screen)
 colours = engine.Colours()
+game = Game()
+
+pygame.mixer.music.play(-1)
 
 # Game loop
 while True:
@@ -33,6 +44,7 @@ while True:
 
     screen.fill(colours.BLACK)
     level.run()
+    game.run()
 
     pygame.display.update()
     clock.tick(FPS_MAX)
